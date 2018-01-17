f3f813384061faaadb9e1a94cdf6449786d384b0
Barry Fossella <bjfossella@gmail.com>
2018-01-17 02:57:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ function sayHiToGrandma(string) {
     return "I can't hear you!";
 } else if (string === uppercase) {
     return "YES INDEED!";
-} else if (string === mixedCase) {
+} else (string === "I love you, Grandma") {
     return "I love you, too.";
 }
 }
