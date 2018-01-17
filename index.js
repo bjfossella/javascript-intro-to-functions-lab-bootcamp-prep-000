f5e83bd503e9973ac70615fe4178f3d67d53ee34
Barry Fossella <bjfossella@gmail.com>
2018-01-17 07:11:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,6 +26,6 @@ function logWhisper(string) {
 }
 
 function sayHiToGranma(string) {
-  if (string.toLowerCase() === string)
+  if (string.toLowerCase() === lowercase)
     return 'I can\'t hear you!';
 }
